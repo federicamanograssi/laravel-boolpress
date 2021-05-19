f3163f86430879396b72e7fcfd720540d1c255dd
Federica <federica.manograssi@gmail.com>
2021-05-19 19:48:41 +0200

    separazione admin/guest
    divisione route & views

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,13 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Auth::routes();
+// Route::get('/home', 'HomeController@index')->name('home');
+
+Route::prefix('admin')
+->namespace('Admin')
+->middleware('auth')
+->group(function () {
+    Route::get('/', 'HomeController@index')->name('admin_homepage');
+});
