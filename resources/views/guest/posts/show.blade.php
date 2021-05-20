d5fbb5416153c3b679a2f475c73523a948fabe75
Federica <federica.manograssi@gmail.com>
2021-05-21 01:59:44 +0200

    Collegamento a Info User

--- resources/views/guest/posts/show.blade.php
+++ resources/views/guest/posts/show.blade.php
@@ -5,6 +5,7 @@
         <div class="row">
             <div class="col-12">
                 <h1>{{$post['title']}}</h1>
+                <h3>Scritto da {{$post->user->name}}</h3>
                 <p>{{$post['content']}}</p>
             </div>
         </div>
